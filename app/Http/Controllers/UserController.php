a0a1cd49fb87d4e7bf1855eea7437e3b841bf6d8
Hadyan Yuma Ekdiattan <hadyanyuma.sekawanmedia@gmail.com>
2024-11-04 18:46:59 +0700

    fresh install

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
